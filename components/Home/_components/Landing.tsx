bd255bbb9903546e7a218370963892c1efae5a4c
realkyr <phuree.kanusont@gmail.com>
2024-12-05 19:53:16 +0700

    feat(landing): create section landing

---
+++ components/Home/_components/Landing.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Section from "@/ui/Section";
+
+const Landing = () => {
+  return (
+    <Section className="flex-col">
+      <h1 className="text-[5rem] font-bold text-center text-primary">
+        ESG HACKATHON PROJECT
+      </h1>
+
+      <h2 className="text-3xl font-bold text-center">
+        BY SME D BANK
+      </h2>
+    </Section>
+  )
+};
+
+export default Landing;
