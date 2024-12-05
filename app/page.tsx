fa907cb23688e7422755ff9cd3fa72717bcdabbf
realkyr <phuree.kanusont@gmail.com>
2024-12-05 19:53:50 +0700

    chore(home): move home to component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,1 @@
-import Navbar from "@/components/Navbar";
-import TopBanner from "@/components/TopBanner";
-
-export default function Home() {
-  return (
-    <>
-      <TopBanner />
-      <Navbar />
-
-      <section className="height-100vh bg-gray-100 flex items-center justify-center">
-      </section>
-    </>
-  );
-}
+export { default } from '@/components/Home'
