ae5c38530f742603089bb30cfdf1d01b2f7cdafd
realkyr <phuree.kanusont@gmail.com>
2024-12-06 00:35:22 +0700

    feat(footer): add image partner for footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Section from "@/ui/Section";
+import Image from "next/image";
 
 const Footer = () => {
 
   return (
-    <Section className="!h-[20dvh] bg-gradient-footer">
+    <Section className="!min-h-[30dvh] bg-gradient-footer flex-col">
       <h1 className="text-4xl font-bold text-center">
         PARTNER
       </h1>
+
+      <div className="grid grid-cols-4 gap-2 mt-2">
+        {[...Array(4)].map((_, index) => (
+          <div className="p-1 rounded-full bg-white w-[90px] h-[90px]">
+            <Image src={`/assets/images/partner-${index + 1}.png`} alt={`Partner ${index+1}`} width={90} height={90} />
+          </div>
+        ))}
+      </div>
     </Section>
   )
 };
