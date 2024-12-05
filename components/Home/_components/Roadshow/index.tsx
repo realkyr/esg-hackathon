c0a982a52ef3abe738a71b8644b2228162846976
realkyr <phuree.kanusont@gmail.com>
2024-12-06 00:26:40 +0700

    feat(roadshow): add image for roadshow

--- components/Home/_components/Roadshow/index.tsx
+++ components/Home/_components/Roadshow/index.tsx
@@ -4,20 +4,35 @@ import Section from "@/ui/Section";
 import EventCardList from "@/components/Home/_components/Roadshow/_components/EventCardList";
 import {events} from "@/components/Home/_components/Roadshow/constants";
 import Container from "@/ui/Container";
+import GreenLeaves from "@/components/Home/_components/Roadshow/_components/GreenLeaves";
 
 const Roadshow = () => {
 
   return (
-    <Section className="flex-col !h-[80dvh]">
-      <Container className="w-full grid grids-cols-1 md:grids-cols-2 gap-4">
-        <div className="col-span-1 md:col-span-2">
+    <Section className="flex-col !min-h-[80dvh]">
+      <Container className="w-full grid grids-cols-1 md:grids-cols-4 gap-4">
+        <div className="col-span-1 md:col-span-4">
           <h1 className="text-4xl font-bold">
             ROADSHOW
           </h1>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <EventCardList events={events} />
+        <div className="col-span-1 md:col-span-3">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <EventCardList events={events}/>
+          </div>
+        </div>
+
+        <div className="col-span-1 flex">
+          <div className="relative h-[350px] w-[350px]">
+            <img src="/assets/images/roadshow-1.png" alt="Roadshow 1" className="h-[350px] w-[350px] inline-block"/>
+
+            <div style={{
+              transform: "translate(50%, 50%)",
+            }}  className="absolute bottom-0 right-0">
+              <GreenLeaves width="100" height="100" />
+            </div>
+          </div>
         </div>
       </Container>
     </Section>
