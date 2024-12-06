de3d1d0aaad86128520eb723de3060b24cbf10dd
realkyr <phuree.kanusont@gmail.com>
2024-12-06 09:30:11 +0700

    ui(app): color adjustment

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -61,15 +61,15 @@ const Navbar: React.FC<NavbarProps> = () => {
         >
           <svg
             xmlns='http://www.w3.org/2000/svg'
-            width='24'
-            height='24'
+            width='30'
+            height='30'
             viewBox='0 0 24 24'
             fill='none'
             stroke='currentColor'
             strokeWidth='2'
             strokeLinecap='round'
             strokeLinejoin='round'
-            className='text-gray-700'
+            className='text-primary fill-primary'
           >
             <line x1='3' y1='6' x2='21' y2='6' />
             <line x1='3' y1='12' x2='21' y2='12' />
